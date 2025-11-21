e6b47b5a5b8404d08e85c024bcba8fe8ca13796c
AliceYang0622 <iamaliceyang.0622@gmail.com>
2025-11-21 11:46:25 +0800

    11/21

---
+++ park/data.js
@@ -0,0 +1,53 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "首頁",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 1.3997684876691778,
+        "pitch": -0.01004085438389879,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.9966177061005226,
+          "pitch": 0.07645034279626906,
+          "title": "<span data-sheets-root=\"1\" style=\"font-size: 10pt; font-family: Arial;\">蘭雅公園</span>",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "首頁",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
